014bd0e515dce4efb94f257784c787a1190887df
brundha093 <158447242+brundha093@users.noreply.github.com>
2024-02-01 13:59:10 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,9 +4,10 @@
   
 from program1 import return_divisible_num
 def main():
-    list1 = []
+   list1 = []
     # open a file
-    file1 = open("input.txt", "r")
+    file1 = open("program1_input.txt", "r")
+
 
     # read the file
     input = file1.read()
@@ -14,9 +15,16 @@ def main():
 
     # close the file
     file1.close()
+    file2 = open("program2_input.txt","r")
+    line = file2.readline()
+    value= line.split("")
+    string_1 = value[0]
+    string_2 = value[1]    
+
     for j in return_divisible_num(int(input)):
         list1.append(j)
   print(list1)
+    
     
 
 
