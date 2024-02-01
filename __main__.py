4e52165fc6cbc0ab9b17bce4cb647ca5555ebf0b
brundha093 <158447242+brundha093@users.noreply.github.com>
2024-02-01 13:29:59 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate 
+# main() function 
+  
+  
+from program1 import *
+
+def main():
+
+  
+  
+# Using the special variable  
+# __name__ 
+if __name__=="__main__": 
+    main() 
