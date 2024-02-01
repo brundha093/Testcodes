42dd6e39e259159ecae7fb882988a44a4da99266
brundha093 <158447242+brundha093@users.noreply.github.com>
2024-02-01 13:30:21 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -3,8 +3,4 @@ def return_divisible_num(num):
         if i % 5 == 0 and i % 7 == 0:
             yield i 
 
-list1 = []
-for j in return_divisible_num(100):
-    list1.append(j)
-    
-print(list1)
+
