c09e6a9682863dd27a03eeab0f3a9c49367f9b4f
brundha093 <158447242+brundha093@users.noreply.github.com>
2024-02-01 14:12:41 +0530

    Create Unittest.py

---
+++ Unittest.py
@@ -0,0 +1,8 @@
+from program1 import return_divisible_num
+from program2 import interleave_two_strings
+
+
+#testing functionality of  return_divisible_num
+return_divisible_num(100) 
+#testing functionality of  interleave_two_strings
+interleave_two_strings("AAAA","12345678")
