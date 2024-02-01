821c132785011871653c46532585806ed7aae38e
brundha093 <158447242+brundha093@users.noreply.github.com>
2024-02-01 13:39:10 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,4 +1,7 @@
 def return_divisible_num(num):
+    """
+    python code to test if number is divisible bt 5 and 7
+    """
     for i in range(1,num):
         if i % 5 == 0 and i % 7 == 0:
             yield i 
